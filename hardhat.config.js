971ed76862c2007e891a04e33c6fc8ec2cfb0bc3
Gyan <gyanprakashdangi@gmail.com>
2024-03-16 08:28:19 +0530

    Server Side setpu done

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,7 +32,16 @@ module.exports = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.17",
+    version: "0.8.9",
+    defaultNetwork: 'sepolia',
+    networks:{
+    sepolia:{
+      // url:'https://rpc.sepolia.org',
+      // url:'https://11155111.rpc.thirdweb.com',
+      url: 'https://rpc.ankr.com/eth_sepolia',
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
     settings: {
       optimizer: {
         enabled: true,
